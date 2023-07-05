b732889ccec3664c37a62f7ab185c81c46778121
OzanYaprak <ozanerhanyaprak@icloud.com>
2023-07-05 22:31:37 +0300

    Add project files.

---
+++ SecimSistemi/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace SecimSistemi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.TBIlceAd = new System.Windows.Forms.TextBox();
+            this.TBBuyukParti = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TBAriPartisi = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TBCabukParti = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.TBDegisimPartisi = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.TBEmekPartisi = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.ButtonOyGiris = new System.Windows.Forms.Button();
+            this.ButtonIstatistik = new System.Windows.Forms.Button();
+            this.ButtonGrafik = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(72, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "İLÇE ADI:";
+            // 
+            // TBIlceAd
+            // 
+            this.TBIlceAd.BackColor = System.Drawing.SystemColors.Info;
+            this.TBIlceAd.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.TBIlceAd.Location = new System.Drawing.Point(150, 12);
+            this.TBIlceAd.Name = "TBIlceAd";
+            this.TBIlceAd.Size = new System.Drawing.Size(171, 27);
+            this.TBIlceAd.TabIndex = 1;
+            // 
+            // TBBuyukParti
+            // 
+            this.TBBuyukParti.BackColor = System.Drawing.SystemColors.Info;
+            this.TBBuyukParti.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.TBBuyukParti.Location = new System.Drawing.Point(150, 105);
+            this.TBBuyukParti.Name = "TBBuyukParti";
+            this.TBBuyukParti.Size = new System.Drawing.Size(171, 27);
+            this.TBBuyukParti.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label2.Location = new System.Drawing.Point(8, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 19);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "BÜYÜK PARTİ:";
+            // 
+            // TBAriPartisi
+            // 
+            this.TBAriPartisi.BackColor = System.Drawing.SystemColors.Info;
+            this.TBAriPartisi.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.TBAriPartisi.Location = new System.Drawing.Point(150, 72);
+            this.TBAriPartisi.Name = "TBAriPartisi";
+            this.TBAriPartisi.Size = new System.Drawing.Size(171, 27);
+            this.TBAriPartisi.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(9, 75);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(98, 19);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "ARI PARTİSİ:";
+            // 
+            // TBCabukParti
+            // 
+            this.TBCabukParti.BackColor = System.Drawing.SystemColors.Info;
+            this.TBCabukParti.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.TBCabukParti.Location = new System.Drawing.Point(150, 138);
+            this.TBCabukParti.Name = "TBCabukParti";
+            this.TBCabukParti.Size = new System.Drawing.Size(171, 27);
+            this.TBCabukParti.TabIndex = 7;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label4.Location = new System.Drawing.Point(9, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(111, 19);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "ÇABUK PARTİ:";
+            // 
+            // TBDegisimPartisi
+            // 
+            this.TBDegisimPartisi.BackColor = System.Drawing.SystemColors.Info;
+            this.TBDegisimPartisi.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.TBDegisimPartisi.Location = new System.Drawing.Point(150, 171);
+            this.TBDegisimPartisi.Name = "TBDegisimPartisi";
+            this.TBDegisimPartisi.Size = new System.Drawing.Size(171, 27);
+            this.TBDegisimPartisi.TabIndex = 9;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label5.Location = new System.Drawing.Point(9, 174);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(134, 19);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "DEĞİŞİM PARTİSİ:";
+            // 
+            // TBEmekPartisi
+            // 
+            this.TBEmekPartisi.BackColor = System.Drawing.SystemColors.Info;
+            this.TBEmekPartisi.ForeColor = System.Drawing.SystemColors.MenuText;
+            this.TBEmekPartisi.Location = new System.Drawing.Point(150, 204);
+            this.TBEmekPartisi.Name = "TBEmekPartisi";
+            this.TBEmekPartisi.Size = new System.Drawing.Size(171, 27);
+            this.TBEmekPartisi.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label6.Location = new System.Drawing.Point(9, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(114, 19);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "EMEK PARTİSİ:";
+            // 
+            // ButtonOyGiris
+            // 
+            this.ButtonOyGiris.BackColor = System.Drawing.Color.OliveDrab;
+            this.ButtonOyGiris.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonOyGiris.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ButtonOyGiris.Location = new System.Drawing.Point(13, 252);
+            this.ButtonOyGiris.Name = "ButtonOyGiris";
+            this.ButtonOyGiris.Size = new System.Drawing.Size(308, 34);
+            this.ButtonOyGiris.TabIndex = 12;
+            this.ButtonOyGiris.Text = "OY GİRİŞİ YAP";
+            this.ButtonOyGiris.UseVisualStyleBackColor = false;
+            // 
+            // ButtonIstatistik
+            // 
+            this.ButtonIstatistik.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonIstatistik.Location = new System.Drawing.Point(15, 292);
+            this.ButtonIstatistik.Name = "ButtonIstatistik";
+            this.ButtonIstatistik.Size = new System.Drawing.Size(144, 34);
+            this.ButtonIstatistik.TabIndex = 13;
+            this.ButtonIstatistik.Text = "İSTATİSTİKLER";
+            this.ButtonIstatistik.UseVisualStyleBackColor = true;
+            // 
+            // ButtonGrafik
+            // 
+            this.ButtonGrafik.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.ButtonGrafik.Location = new System.Drawing.Point(177, 292);
+            this.ButtonGrafik.Name = "ButtonGrafik";
+            this.ButtonGrafik.Size = new System.Drawing.Size(144, 34);
+            this.ButtonGrafik.TabIndex = 14;
+            this.ButtonGrafik.Text = "GRAFİKLER";
+            this.ButtonGrafik.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(348, 343);
+            this.Controls.Add(this.ButtonGrafik);
+            this.Controls.Add(this.ButtonIstatistik);
+            this.Controls.Add(this.ButtonOyGiris);
+            this.Controls.Add(this.TBEmekPartisi);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.TBDegisimPartisi);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.TBCabukParti);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.TBAriPartisi);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.TBBuyukParti);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.TBIlceAd);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox TBIlceAd;
+        private System.Windows.Forms.TextBox TBBuyukParti;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox TBAriPartisi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TBCabukParti;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TBDegisimPartisi;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TBEmekPartisi;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button ButtonOyGiris;
+        private System.Windows.Forms.Button ButtonIstatistik;
+        private System.Windows.Forms.Button ButtonGrafik;
+    }
+}
+
